2c95b832ea65beb80d04396bb5d32dabe2d5eee9
Bruce Walker <8148028+bdwalker1@users.noreply.github.com>
2024-03-23 15:12:42 -0700

    Reworked to eliminate some redundant code

--- WordleHelper/wordleHelper.py
+++ WordleHelper/wordleHelper.py
@@ -10,18 +10,19 @@ this.valid_words = list()
 
 
 def readfiles():
-    raw_df = pd.read_csv("http://raw.githubusercontent.com/bdwalker1/UCSD_ML_Bootcamp/main/WordleHelper/wordle_valid_words.txt")
+    raw_df = pd.read_csv(
+                "https://raw.githubusercontent.com/bdwalker1/UCSD_ML_Bootcamp/main/WordleHelper/wordle_valid_words.txt"
+                )
     this.valid_words = list(raw_df['valid_word'])
     del raw_df
-    raw_df = pd.read_csv("http://raw.githubusercontent.com/bdwalker1/UCSD_ML_Bootcamp/main/WordleHelper/wordle_used_words.txt")
+    raw_df = pd.read_csv(
+                "https://raw.githubusercontent.com/bdwalker1/UCSD_ML_Bootcamp/main/WordleHelper/wordle_used_words.txt"
+                )
     this.used_words = list(raw_df['used_word'])
     del raw_df
 
 
-def matchunusedwords(pattern: str, keep_ltrs:str = '', elim_ltrs: str = '') -> list:
-    """Given a RegEx pattern and a string of eliminated letters,
-        return matching words from the valid Wordle word list
-        that have not yet been used"""
+def parseparams(pattern, keep_ltrs, elim_ltrs) -> tuple:
     ptrn = pattern.lower()
     in_ltrs = keep_ltrs.lower()
     ex_ltrs = elim_ltrs.lower()
@@ -32,58 +33,49 @@ def matchunusedwords(pattern: str, keep_ltrs:str = '', elim_ltrs: str = '') -> l
     for c in ex_ltrs:
         if c in pattern:
             raise ValueError("Your pattern contains one of the elimination characters")
+    return ptrn, in_ltrs, ex_ltrs
 
-    readfiles()
-    possible_words = []
-    unused_words = [word for word in this.valid_words if word not in this.used_words]
-    match_words = [word for word in unused_words if re.search(ptrn, word) is not None]
 
-    for word in match_words:
+def findmatchingwords(wordlist, keep_ltrs, elim_ltrs) -> list:
+    matches = list()
+    for word in wordlist:
         word_good: bool = True
-        for c in ex_ltrs:
+        for c in elim_ltrs:
             if c in word:
                 word_good = False
                 break
-        for c in in_ltrs:
+        for c in keep_ltrs:
             if c not in word:
                 word_good = False
                 break
         if word_good:
-            possible_words.append(word)
+            matches.append(word)
+    return matches
+
+
+def matchunusedwords(pattern: str, keep_ltrs: str = '', elim_ltrs: str = '') -> list:
+    """Given a RegEx pattern and a string of eliminated letters,
+        return matching words from the valid Wordle word list
+        that have not yet been used"""
+
+    ptrn, in_ltrs, ex_ltrs = parseparams(pattern, keep_ltrs, elim_ltrs)
+    readfiles()
+    unused_words = [word for word in this.valid_words if word not in this.used_words]
+    match_words = [word for word in unused_words if re.search(ptrn, word) is not None]
+    possible_words = findmatchingwords(match_words, in_ltrs, ex_ltrs)
 
     return possible_words
 
 
-def matchwords(pattern: str, keep_ltrs:str = '', elim_ltrs: str = '') -> list:
+def matchwords(pattern: str, keep_ltrs: str = '', elim_ltrs: str = '') -> list:
     """Given a RegEx pattern and a string of eliminated letters,
         return matching words from the valid Wordle word list"""
-    ptrn = pattern.lower()
-    in_ltrs = keep_ltrs.lower()
-    ex_ltrs = elim_ltrs.lower()
 
-    if len(ptrn) != 5:
-        raise ValueError("The RegEx pattern must be 5 characters long")
-
-    for c in ex_ltrs:
-        if c in ptrn:
-            raise ValueError("Your pattern contains one of the elimination characters")
-
+    ptrn, in_ltrs, ex_ltrs = parseparams(pattern, keep_ltrs, elim_ltrs)
     readfiles()
     match_words = [word for word in this.valid_words if re.search(ptrn, word) is not None]
 
-    matching_words = []
-    for word in match_words:
-        word_good: bool = True
-        for c in ex_ltrs:
-            if c in word:
-                word_good = False
-                break
-        for c in in_ltrs:
-            if c not in word:
-                word_good = False
-                break
-        if word_good:
-            matching_words.append(word)
+    matching_words = findmatchingwords(match_words, in_ltrs, ex_ltrs)
 
     return matching_words
 
